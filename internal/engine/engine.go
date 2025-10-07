6d62e27792848a8f6d46ceef3b72d495f60e4f65
Ennio Simões <ennio.simoes@outlook.com>
2025-10-07 12:24:21 -0300

    add sync comand

--- internal/engine/engine.go
+++ internal/engine/engine.go
@@ -9,7 +9,18 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
+func joinColumns(columns []string, separator string) string {
+	result := ""
+	for i, column := range columns {
+		if i > 0 {
+			result += separator
+		}
+		result += column
+	}
+	return result
+}
+
+func Sync() {
 	dbSource, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/database_source")
 	if err != nil {
 		panic(err)
@@ -21,95 +32,104 @@ func main() {
 		panic(err)
 	}
 	defer dbDestination.Close()
-	// createConfigFile()
-	// configSource, err := getConfig("destination")
-	// if err != nil {
-	// 	println("Erro ao obter a configuração de origem")
-	// 	return
-	// }
-	// println("Configuração de origem:")
-	// fmt.Println(configSource)
 
-	// getSourceTables(dbSource)
-	// getSourceTables(dbDestination)
+	cfg, err := config.GetConfigFromFile()
+	if err != nil {
+		fmt.Println("Error getting configuration:", err)
+		return
+	}
+
+	tablesToSync := cfg.Tables
+
+	// disable FOREIGN_KEY_CHECKS
+	_, err = dbDestination.Exec("SET FOREIGN_KEY_CHECKS = 0")
+	if err != nil {
+		fmt.Printf("Error disabling FOREIGN_KEY_CHECKS in destination: %v\n", err)
+		return
+	}
+
+	for _, tableName := range tablesToSync {
+		// go func(tableName string) {
+		fmt.Printf("\nProcessing table: %s ...\n", tableName)
+
+		// Select all data from source database
+		rows, err := dbSource.Query(fmt.Sprintf("SELECT * FROM %s", tableName))
+		if err != nil {
+			fmt.Printf("Error selecting data from table %s in source: %v\n", tableName, err)
+			return
+		}
+		defer rows.Close()
+
+		columns, err := rows.Columns()
+		if err != nil {
+			fmt.Printf("Error getting columns for table %s: %v\n", tableName, err)
+			return
+		}
+
+		placeholders := ""
+		for i := 0; i < len(columns); i++ {
+			if i > 0 {
+				placeholders += ", "
+			}
+			placeholders += "?"
+		}
+
+		// TRUCATE TABLE
+		_, err = dbDestination.Exec(fmt.Sprintf("TRUNCATE TABLE %s", tableName))
+		fmt.Printf("Truncating table %s in destination...\n", tableName)
+		if err != nil {
+			fmt.Printf("Error truncating table %s in destination: %v\n", tableName, err)
+			return
+		}
+
+		query := fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s)", tableName, joinColumns(columns, ","), placeholders)
+
+		insertStmt, err := dbDestination.Prepare(query)
+		if err != nil {
+			fmt.Printf("Error preparing insert statement for table %s: %v\n", tableName, err)
+			return
+		}
+		defer insertStmt.Close()
+
+		// Loop through rows and insert into destination
+		countRows := 0
+		for rows.Next() {
+			// Create a slice of interface{} to hold the column values
+			values := make([]interface{}, len(columns))
+			for i := range values {
+				values[i] = new(interface{})
+			}
+
+			if err := rows.Scan(values...); err != nil {
+				fmt.Printf("Error scanning row for table %s: %v\n", tableName, err)
+				continue
+			}
+
+			// Insert the row into the destination database
+			if _, err := insertStmt.Exec(values...); err != nil {
+				fmt.Printf("Error inserting row into table %s in destination: %v\n", tableName, err)
+				continue
+			}
+			countRows++
+		}
+
+		if err := rows.Err(); err != nil {
+			fmt.Printf("Error iterating over rows for table %s: %v\n", tableName, err)
+			return
+		}
+
+		fmt.Printf("Successfully synced a total: %d rows in table %s\n", countRows, tableName)
+	}
+
+	// enable FOREIGN_KEY_CHECKS
+	_, err = dbDestination.Exec("SET FOREIGN_KEY_CHECKS = 1")
+	if err != nil {
+		fmt.Printf("Error enabling FOREIGN_KEY_CHECKS in destination: %v\n", err)
+		return
+	}
+
+	fmt.Println("\nSync completed successfully!")
 }
-
-// type Database struct {
-// 	Host     string `json:"host"`
-// 	Port     int    `json:"port"`
-// 	Username string `json:"username"`
-// 	Password string `json:"password"`
-// 	Database string `json:"database"`
-// }
-
-// type Config struct {
-// 	DatabaseSource      Database `json:"database_source"`
-// 	DatabaseDestination Database `json:"database_destination"`
-// }
-
-// func createConfigFile() {
-// 	file, err := os.Create("synkgo.json")
-// 	if err != nil {
-// 		println("Erro ao criar o arquivo")
-// 	}
-// 	defer file.Close()
-
-// 	config := dto.Config{
-// 		DatabaseSource: dto.Database{
-// 			Host:     "localhost",
-// 			Port:     "5432",
-// 			Username: "user",
-// 			Password: "password",
-// 			Database: "source_db",
-// 		},
-// 		DatabaseDestination: dto.Database{
-// 			Host:     "localhost",
-// 			Port:     "5432",
-// 			Username: "user",
-// 			Password: "password",
-// 			Database: "destination_db",
-// 		},
-// 	}
-
-// 	data, err := json.MarshalIndent(config, "", "  ")
-// 	if err != nil {
-// 		println("Erro ao formatar o JSON")
-// 		return
-// 	}
-// 	_, err = file.WriteString(string(data))
-// 	if err != nil {
-// 		println("Erro ao escrever no arquivo")
-// 	}
-// 	defer file.Close()
-// 	println("Arquivo criado com sucesso")
-// }
-
-// func getConfig(source string) (dto.Database, error) {
-// 	file, err := os.Open("./synkgo.json")
-// 	if err != nil {
-// 		println("Erro ao abrir o arquivo")
-// 		return dto.Database{}, err
-// 	}
-// 	defer file.Close()
-
-// 	var config dto.Config
-// 	decoder := json.NewDecoder(file)
-// 	err = decoder.Decode(&config)
-// 	if err != nil {
-// 		println("Erro ao decodificar o JSON")
-// 		return dto.Database{}, err
-// 	}
-
-// 	if source == "source" {
-// 		return config.DatabaseSource, nil
-// 	}
-
-// 	if source == "destination" {
-// 		return config.DatabaseDestination, nil
-// 	}
-
-// 	return dto.Database{}, nil
-// }
 
 func getTables(db *sql.DB) ([]string, error) {
 	rows, err := db.Query("SHOW TABLES")
